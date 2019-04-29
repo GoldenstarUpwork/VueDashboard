1a5fddccc14160521aa56556acb05c8ea5a7c135
GoldenstarUpwork <juvencocastro@gmail.com>
2019-04-28 18:56:05 -0700

    Added donut / pie charts

--- src/components/views/home/cardChart.vue
+++ src/components/views/home/cardChart.vue
@@ -2,17 +2,12 @@
   <div class="card bg-white animate-scale ">
     <div class="card-title bg-teal text-white">
       {{cardTitle}}
-      <div class=" float-right">
-        <q-fab icon="keyboard_arrow_left" direction="left"  classNames="primary">
-          <q-small-fab class="teal" @click.native="type = 'bar'" icon=""><i>equalizer</i></q-small-fab>
-          <q-small-fab class="teal" @click.native="type = 'horizontalBar'" icon=""><i class="horizontal-icon">equalizer</i></q-small-fab>
-          <q-small-fab class="teal" @click.native="type = 'pie'" icon=""><i>pie_chart_outlined</i></q-small-fab>
-          <q-small-fab class="teal" @click.native="type = 'doughnut'" icon=""><i>donut_large</i></q-small-fab>
-        </q-fab>
-        <button class="primary circular raised " @click="toImage()">
-          <i>portrait</i>
-        </button>
-      </div>
+      <q-fab icon="keyboard_arrow_left" direction="left" class="float-right" classNames="primary">
+        <q-small-fab class="teal" @click.native="type = 'bar'" icon=""><i>equalizer</i></q-small-fab>
+        <q-small-fab class="teal" @click.native="type = 'horizontalBar'" icon=""><i class="horizontal-icon">equalizer</i></q-small-fab>
+        <q-small-fab class="teal" @click.native="type = 'pie'" icon=""><i>pie_chart_outlined</i></q-small-fab>
+        <q-small-fab class="teal" @click.native="type = 'doughnut'" icon=""><i>donut_large</i></q-small-fab>
+      </q-fab>
     </div>
     <div class="card-content">
       <canvas ref="chart"></canvas>
@@ -88,9 +83,6 @@
               }
             }
           })
-      },
-      toImage () {
-        window.open(this.chart.toBase64Image());
       }
     }
   }
