4baa26f8ce969eb4c1e8f9022afd18347756ad32
GoldenstarUpwork <juvencocastro@gmail.com>
2019-04-28 18:46:08 -0700

    Update LICENSE

---
+++ src/components/layout/drawer.vue
@@ -0,0 +1,24 @@
+<template>
+  <q-drawer ref="leftDrawer">
+    <div class="list no-border platform-delimiter">
+      <div class="list-label ">Dashboard</div>
+      <q-drawer-link icon="home" :to="{path: '/dash', exact: true}">
+        Dashboard One
+      </q-drawer-link>
+      <div class="list-label ">Forms</div>
+      <q-drawer-link icon="search" :to="{path: '/form', exact: true}">
+        Form find / edit
+      </q-drawer-link>
+
+    </div>
+  </q-drawer>
+</template>
+<script>
+  export default {
+  }
+</script>
+<style scoped>
+  .toolbar {
+    min-height: 70px;
+  }
+</style>
